8abac4fb1793aed1d773497f2e6be0846572cedc
Neil Swart <neilswart3@gmail.com>
2021-11-12 17:24:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Create React App boilerplate
+
+This project includes the following
+
+- Typescript
+- PWA
+
+All set up so you can just start coding
---
+++ src/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Home, Genres, Films } from 'src/components'
+import { GeneralLayout } from 'src/layouts'
+
+function App() {
+  return (
+    <div>
+      <h1>Create React App - Typescript - PWA boilerplate</h1>
+    </div>
+  )
+}
+
+export default App
